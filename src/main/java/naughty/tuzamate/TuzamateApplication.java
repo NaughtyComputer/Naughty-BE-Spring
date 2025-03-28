32c8da24362c8f577ce325e623e36468582872b1
RCNR <iop04226@gmail.com>
2025-03-28 13:35:02 +0900

    first commit

---
+++ src/main/java/naughty/tuzamate/TuzamateApplication.java
@@ -0,0 +1,13 @@
+package naughty.tuzamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuzamateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuzamateApplication.class, args);
+	}
+
+}
