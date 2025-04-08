468d7bb86e19d19e64a4f76c6acdc95dcfd8c678
RCNR <iop04226@gmail.com>
2025-04-08 15:00:56 +0900

    chore(UserErrorCode): 에러명 변경

--- src/main/java/naughty/tuzamate/domain/user/error/UserErrorCode.java
+++ src/main/java/naughty/tuzamate/domain/user/error/UserErrorCode.java
@@ -12,8 +12,8 @@ public enum UserErrorCode implements BaseErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER40402", "해당 회원이 존재하지 않습니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER40901", "회원이 이미 존재합니다."),
     USER_PASSWORD_INCORRECT(HttpStatus.CONFLICT, "USER40101", "패스워드가 올바르지 않습니다"),
-    OAUTH_TOKEN_FAIL(HttpStatus.UNAUTHORIZED, "USER401", "인가코드로 토큰을 가져오는데 실패했습니다."),
-    OAUTH_USER_INFO_FAIL(HttpStatus.UNAUTHORIZED, "USER401", "토큰으로 사용자 정보를 가져오는 데 실패했습니다."),
+    OAUTH_TOKEN_FAIL(HttpStatus.UNAUTHORIZED, "USER40101", "인가코드로 토큰을 가져오는데 실패했습니다."),
+    OAUTH_USER_INFO_FAIL(HttpStatus.UNAUTHORIZED, "USER40102", "토큰으로 사용자 정보를 가져오는 데 실패했습니다."),
     UNSUPPORTED_OAUTH_TYPE(HttpStatus.BAD_REQUEST, "USER400", "지원하지 않는 소셜 로그인입니다."),
     ;
 
