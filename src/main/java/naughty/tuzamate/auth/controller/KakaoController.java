e550e3e6c87655aebe1ab181bce60c05a65fb942
RCNR <iop04226@gmail.com>
2025-04-08 14:46:59 +0900

    feat(KakaoOAuth2): 카카오 소셜 로그인 관련 로직

---
+++ src/main/java/naughty/tuzamate/auth/controller/KakaoController.java
@@ -0,0 +1,24 @@
+package naughty.tuzamate.auth.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import naughty.tuzamate.auth.service.OAuth2Service;
+import naughty.tuzamate.global.apiPayload.CustomResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "카카오 로그인")
+public class KakaoController {
+
+    private final OAuth2Service oAuth2Service;
+
+    @GetMapping("/oauth2/tuzamate/kakao")
+    // code는 카카오에서 주는 것 (디버깅으로 확인 가능)
+    public CustomResponse<?> KakaoLogin(@RequestParam("code") String code) {
+
+        return CustomResponse.onSuccess(oAuth2Service.login("kakao", code));
+    }
+}
