ed3dcf0fbf004cb7936018efb28ee75e64b01d4a
RCNR <iop04226@gmail.com>
2025-05-02 23:16:43 +0900

    fix: 중복 UserRepository 제거

--- src/main/java/naughty/tuzamate/domain/user/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package naughty.tuzamate.domain.user.domain.repository;
-
-import naughty.tuzamate.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
