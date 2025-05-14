23f9d35c0e35b43080e8b42a85207bf8dd1773e8
donghyukseo <ehdgurdusdn@naver.com>
2025-05-15 00:46:43 +0900

    feat : 적금(savings) Entity 및 기능 추가

---
+++ src/main/java/naughty/tuzamate/domain/savings/entity/Savings.java
@@ -0,0 +1,35 @@
+package naughty.tuzamate.domain.savings.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "deposit")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Savings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String korCoNm;       // 금융 회사명
+
+    private String finPrdtNm;     // 상품명
+
+    private String joinWay;       // 가입 방법
+
+    private String joinMember;    // 가입 대상
+
+    @Column(length = 1000)
+    private String mtrtInt;       // 만기 후 이자율
+
+    @Column(length = 1000)
+    private String spclCnd;       // 우대 조건
+
+    @Column(length = 1000)
+    private String etcNote;       // 기타 유의사항
+}
