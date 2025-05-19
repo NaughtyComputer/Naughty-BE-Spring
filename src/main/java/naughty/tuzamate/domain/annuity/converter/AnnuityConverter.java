8e1bccc962bd18b06fa4630b77ae9d386cf9a81b
donghyukseo <ehdgurdusdn@naver.com>
2025-05-19 12:04:07 +0900

    feat : annuity 기능 추가

--- src/main/java/naughty/tuzamate/domain/annuity/converter/AnnuityConverter.java
+++ src/main/java/naughty/tuzamate/domain/annuity/converter/AnnuityConverter.java
@@ -10,14 +10,14 @@ public class AnnuityConverter {
         return Annuity.builder()
                 .korCoNm(reqDTO.kor_co_nm())
                 .finPrdtNm(reqDTO.fin_prdt_nm())
-                .pnsnKindNm(reqDTO.pnsnKindNm())
-                .prdtTypeNm(reqDTO.prdtTypeNm())
-                .avgPrftRate(reqDTO.avgPrftRate())
-                .guarRate(reqDTO.guarRate())
-                .btrmPrftRate1(reqDTO.btrmPrftRate1())
-                .joinWay(reqDTO.joinWay())
-                .saleCo(reqDTO.saleCo())
-                .saleStrtDay(reqDTO.saleStrtDay())
+                .pnsnKindNm(reqDTO.pnsn_kind_nm())
+                .prdtTypeNm(reqDTO.prdt_type_nm())
+                .avgPrftRate(reqDTO.avg_prft_rate())
+                .guarRate(reqDTO.guar_rate())
+                .btrmPrftRate1(reqDTO.btrm_prft_rate_1())
+                .joinWay(reqDTO.join_way())
+                .saleCo(reqDTO.sale_co())
+                .saleStrtDay(reqDTO.sale_strt_day())
                 .build();
     }
 }
