c6e001e99c49fcc6e5ea07c9c0b8f7d7d685576d
RCNR <iop04226@gmail.com>
2025-06-24 22:52:06 +0900

    feat: UserInfo 어노테이션 생성

--- src/main/java/naughty/tuzamate/domain/user/error/UserErrorCode.java
+++ src/main/java/naughty/tuzamate/domain/user/error/UserErrorCode.java
@@ -15,6 +15,7 @@ public enum UserErrorCode implements BaseErrorCode {
     OAUTH_TOKEN_FAIL(HttpStatus.UNAUTHORIZED, "USER40101", "인가코드로 토큰을 가져오는데 실패했습니다."),
     OAUTH_USER_INFO_FAIL(HttpStatus.UNAUTHORIZED, "USER40102", "토큰으로 사용자 정보를 가져오는 데 실패했습니다."),
     UNSUPPORTED_OAUTH_TYPE(HttpStatus.BAD_REQUEST, "USER400", "지원하지 않는 소셜 로그인입니다."),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "USER401", "인증되지 않은 사용자입니다.")
     ;
 
 
