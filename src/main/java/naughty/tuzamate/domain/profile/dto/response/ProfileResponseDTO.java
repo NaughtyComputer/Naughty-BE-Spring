100cd827ac245740c61e318511eceaa4b6485f47
RCNR <iop04226@gmail.com>
2025-06-24 22:52:45 +0900

    refactor: 유저 프로필 조회 api 관련 내용 수정

--- src/main/java/naughty/tuzamate/domain/profile/dto/response/ProfileResponseDTO.java
+++ src/main/java/naughty/tuzamate/domain/profile/dto/response/ProfileResponseDTO.java
@@ -1,10 +1,31 @@
 package naughty.tuzamate.domain.profile.dto.response;
 
-public record ProfileResponseDTO(
-        String nickname
+import lombok.Builder;
 
-) {
+public class ProfileResponseDTO {
 
+    @Builder
+    public record updateProfileResponse(
+            String nickname
+    ) {}
+
+    @Builder
+    public record getProfileResponse(
+            Long age,
+            boolean experience,
+            String email,
+            String nickname,
+            String funding_situation,
+            Long income,
+            String stability,
+            String source,
+            String type,
+            String proportion,
+            Long period,
+            Long expected_income,
+            Long expected_loss,
+            String purpose
+    ) {}
 
 
 }
--- src/main/java/naughty/tuzamate/domain/user/dto/UserResponseDTO.java
+++ src/main/java/naughty/tuzamate/domain/user/dto/UserResponseDTO.java
@@ -16,48 +16,4 @@ public class UserResponseDTO {
 
     }
 
-
-    @Getter
-    @Builder
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public static class UserProfileDTO {
-
-        private Long age;
-        private boolean experience;
-        private String email;
-        private String nickname;
-        private String funding_situation;
-        private Long income;
-        private String stability;
-        private String source;
-        private String type;
-        private String proportion;
-        private Long period;
-        private Long expected_income;
-        private Long expected_loss;
-        private String purpose;
-
-        public static UserProfileDTO from(User user) {
-
-            return UserProfileDTO.builder()
-                    .age(user.getAge())
-                    .experience(user.isExperience())
-                    .email(user.getEmail())
-                    .nickname(user.getNickname())
-                    .funding_situation(user.getFunding_situation())
-                    .income(user.getIncome())
-                    .stability(user.getStability())
-                    .source(user.getSource())
-                    .type(user.getType())
-                    .proportion(user.getProportion())
-                    .period(user.getPeriod())
-                    .expected_income(user.getExpected_income())
-                    .expected_loss(user.getExpected_loss())
-                    .purpose(user.getPurpose())
-                    .build();
-        }
-
-
-    }
 }
