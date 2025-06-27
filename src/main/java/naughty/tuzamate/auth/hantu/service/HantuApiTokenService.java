339aea9db417230236203e4f81cd168f770913c6
RCNR <iop04226@gmail.com>
2025-06-28 03:15:55 +0900

    chore: 한투 액세스 토큰 코드 변경

--- src/main/java/naughty/tuzamate/auth/hantu/service/HantuApiTokenService.java
+++ src/main/java/naughty/tuzamate/auth/hantu/service/HantuApiTokenService.java
@@ -53,7 +53,7 @@ public class HantuApiTokenService {
             ResponseEntity<ResponseHantuAccessTokenDto> response = restTemplate.postForEntity(tokenUrl, request, ResponseHantuAccessTokenDto.class);
 
             if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
-                apiTokenStore.saveAccessToken(response.getBody().getAccessToken());
+                apiTokenStore.saveAccessToken(response.getBody().getAccess_token());
                 log.info("HanTU Access Token 갱신 완료");
                 return true;
             }
