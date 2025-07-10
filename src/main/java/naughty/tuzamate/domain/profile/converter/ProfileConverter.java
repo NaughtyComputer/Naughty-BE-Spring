d0ae88cd3d4884ef190fff626977d86754e11da1
RCNR <iop04226@gmail.com>
2025-07-11 00:47:52 +0900

    feat: 커뮤니티 확인 기능 추가 - 사용자의 스크랩 목록 확인

--- src/main/java/naughty/tuzamate/domain/profile/converter/ProfileConverter.java
+++ src/main/java/naughty/tuzamate/domain/profile/converter/ProfileConverter.java
@@ -1,7 +1,13 @@
 package naughty.tuzamate.domain.profile.converter;
 
+import naughty.tuzamate.domain.post.entity.Post;
+import naughty.tuzamate.domain.postScrap.entity.PostScrap;
+import naughty.tuzamate.domain.profile.dto.request.ProfileRequestDTO;
 import naughty.tuzamate.domain.profile.dto.response.ProfileResponseDTO;
 import naughty.tuzamate.domain.user.entity.User;
+import org.springframework.data.domain.Slice;
+
+import java.util.ArrayList;
 
 public class ProfileConverter {
 
@@ -31,4 +37,25 @@ public class ProfileConverter {
                 .purpose(user.getPurpose())
                 .build();
     }
+
+    public static ProfileResponseDTO.scrapResponse toPostScrapResponseDTO(PostScrap postScrap) {
+
+        return ProfileResponseDTO.scrapResponse.builder()
+                .postId(postScrap.getId())
+                .title(postScrap.getPost().getTitle())
+                .contentPreview(postScrap.getPost().getContent().length() > 20 ? postScrap.getPost().getContent().substring(0, 20) + "..." : postScrap.getPost().getContent())
+                .build();
+    }
+
+    public static ProfileResponseDTO.scrapListResponse toScrapListResponse(Slice<PostScrap> postScraps) {
+        return ProfileResponseDTO.scrapListResponse.builder()
+                .scraps(postScraps.getContent().isEmpty() ? new ArrayList<>() :
+                        postScraps.getContent().stream()
+                                .map(postScrap -> toPostScrapResponseDTO(postScrap))
+                                .toList())
+                .hasNextPage(postScraps.hasNext())
+                .cursor(postScraps.getContent().isEmpty() ? 0L : postScraps.getContent().get(postScraps.getContent().size() - 1).getId())
+                .build();
+    }
+
 }
---
+++ src/test/java/naughty/tuzamate/domain/profile/service/query/ProfileQueryServiceTest.java
@@ -0,0 +1,118 @@
+package naughty.tuzamate.domain.profile.service.query;
+
+import naughty.tuzamate.domain.post.entity.Post;
+import naughty.tuzamate.domain.postScrap.entity.PostScrap;
+import naughty.tuzamate.domain.postScrap.repository.PostScrapRepository;
+import naughty.tuzamate.domain.profile.converter.ProfileConverter;
+import naughty.tuzamate.domain.profile.dto.response.ProfileResponseDTO;
+import naughty.tuzamate.domain.user.entity.User;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import java.util.List;
+
+import static org.awaitility.Awaitility.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProfileQueryServiceTest {
+
+    @Mock
+    private PostScrapRepository postScrapRepository;
+
+    @InjectMocks
+    private ProfileQueryService profileQueryService;
+
+    @Test
+    @DisplayName("cursor != 0 일 때 scrapList 조회")
+    void scrapList_cursor_positive() {
+
+        // given
+        // 유저 생성
+        User user = User.builder().id(1L).build();
+
+        // 옵셋 및 cursor 설정
+        int offset = 10;
+        Long cursor = 5L;
+
+
+        SliceImpl<PostScrap> postScraps = new SliceImpl<>(
+                List.of(
+                        PostScrap.builder().id(1L).post(Post.builder().id(1L).title("title1").content("content1").build()).build(),
+                        PostScrap.builder().id(2L).post(Post.builder().id(2L).title("title2").content("content2").build()).build()
+                ),
+                PageRequest.of(0, offset),
+                false
+        );
+
+        when(postScrapRepository.findScrapListByUserIdLessThanOrderByIdDesc(eq(user.getId()), eq(cursor), any()))
+                .thenReturn(postScraps);
+
+        ProfileResponseDTO.scrapListResponse expectedResponse = new ProfileResponseDTO.scrapListResponse(List.of(), false, 5L);
+
+        try (MockedStatic<ProfileConverter> mocked =
+                     mockStatic(ProfileConverter.class)) {
+            mocked.when(() -> ProfileConverter.toScrapListResponse(postScraps))
+                    .thenReturn(expectedResponse);
+
+            // when
+            ProfileResponseDTO.scrapListResponse actualResponse = profileQueryService.getScrapList(user, cursor, offset);
+
+            // then
+            Assertions.assertThat(actualResponse).isSameAs(expectedResponse);
+        }
+
+    }
+
+    @Test
+    @DisplayName("cursor != 0일 때 scrapList 조회 - 실제 동작")
+    void scrapList_cursor_positive_real() {
+
+        // given
+        // 유저 생성
+        User user = User.builder().id(1L).build();
+
+        // 옵셋 및 cursor 설정
+        int offset = 10;
+        Long cursor = 5L;
+
+        SliceImpl<PostScrap> postScraps = new SliceImpl<>(
+                List.of(
+                        PostScrap.builder().id(1L).post(Post.builder().id(1L).title("title1").content("content1").build()).build(),
+                        PostScrap.builder().id(2L).post(Post.builder().id(2L).title("title2").content("content2").build()).build()
+                ),
+                PageRequest.of(0, offset),
+                false
+        );
+
+        when(postScrapRepository.findScrapListByUserIdLessThanOrderByIdDesc(eq(user.getId()), eq(cursor), any()))
+                .thenReturn(postScraps);
+
+        // when
+        ProfileResponseDTO.scrapListResponse actualResponse = profileQueryService.getScrapList(user, cursor, offset);
+
+        // then
+        Assertions.assertThat(actualResponse.scraps())
+                .extracting("postId", "title", "contentPreview")
+                .containsExactly(
+                        Tuple.tuple(1L, "title1", "content1"),
+                        Tuple.tuple(2L, "title2", "content2")
+                );
+
+        Assertions.assertThat(actualResponse.hasNextPage()).isFalse();
+        Assertions.assertThat(actualResponse.cursor()).isEqualTo(2L); // 마지막 PostScrap의 id가 2이므로 cursor는 2L이어야 함
+    }
+}
