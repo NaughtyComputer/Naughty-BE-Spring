ba37aa5b02c1af9b1f93e30a7823d4f9a59fdff7
RCNR <iop04226@gmail.com>
2025-07-29 00:17:57 +0900

    feat: 한국 주식 저장 트랜잭션 설정

--- src/main/java/naughty/tuzamate/domain/stock/service/KrxService.java
+++ src/main/java/naughty/tuzamate/domain/stock/service/KrxService.java
@@ -26,6 +26,7 @@ public class KrxService {
     private final KrxStockInfoRepository krxStockInfoRepository;
     private final AsyncKrxStockFetcher asyncKrxStockFetcher;
 
+    @Transactional
     public void saveKrxStocksInfo() {
         log.info("한국 주식 정보 저장/업데이트 시작");
         long start = System.currentTimeMillis();
@@ -44,9 +45,11 @@ public class KrxService {
         log.info("{}개의 한국 주식 정보 요청 시작", stockCodeList.size());
 
 
+        // 모든 CompletableFuture가 완료될 때까지 기다린다
         CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0])).join(); // join은 예외를 던지지 않는다
         log.info("모든 한국 주식 정보 요청 완료");
 
+        // 결과를 Optional<KrxStockInfo>로 변환하여 리스트로 수집한다
         List<KrxStockInfo> stockInfoList = completableFutures.stream()
                 .map(CompletableFuture::join) // CompletableFuture에서 결과를 가져온다 -> Optional<KrxStockInfo>
                 .filter(Optional::isPresent) // 비어있지 않은 경우만
