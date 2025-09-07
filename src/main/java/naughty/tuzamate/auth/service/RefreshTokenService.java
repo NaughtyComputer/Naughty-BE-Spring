e696b76c37bd0c623fa430bb2edf60a50a4b8dca
RCNR <iop04226@gmail.com>
2025-09-07 12:08:03 +0900

    refactor: 리플레시 토큰 레디스 적용 - 리플레시 토큰 저장을 db가 아닌 레디스 이용

--- src/main/java/naughty/tuzamate/auth/service/RefreshTokenService.java
+++ src/main/java/naughty/tuzamate/auth/service/RefreshTokenService.java
@@ -1,38 +1,59 @@
 package naughty.tuzamate.auth.service;
 
+import io.netty.util.internal.StringUtil;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import naughty.tuzamate.auth.entity.RefreshToken;
 import naughty.tuzamate.auth.repository.RefreshTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import javax.swing.text.html.Option;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional
+//@Transactional
 public class RefreshTokenService {
 
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+    private ValueOperations<String, String> valueOperations;
 
-    public void saveRefreshToken(Long userId, String token, LocalDateTime expire) {
-
-        deleteToken(userId);
-
-        RefreshToken refreshToken = new RefreshToken(token, userId, expire);
-        refreshTokenRepository.save(refreshToken);
+    @PostConstruct
+    private void init() {
+        valueOperations = redisTemplate.opsForValue();
     }
 
-    public boolean validateToken(String token) {
+    public void saveRefreshToken(Long userId, String token, LocalDateTime expire) {
+        String key = "refreshToken:" + userId;
+        Duration ttl = Duration.between(LocalDateTime.now(), expire);
+        valueOperations.set(key, token, ttl);
+    }
+
+    public Optional<String> getRefreshToken(Long userId) {
+        String key = "refreshToken:" + userId;
+        String token = valueOperations.get(key);
+
+        return StringUtils.hasText(token) ? Optional.of(token) : Optional.empty();
+
+    }
+
+    public void deleteRefreshToken(Long userId) {
+        String key = "refreshToken:" + userId;
+        redisTemplate.delete(key);
+    }
+
+   /* public boolean validateToken(String token) {
 
         Optional<RefreshToken> refreshToken = refreshTokenRepository.findByToken(token);
         return refreshToken.isPresent() && refreshToken.get().getExpireDate().isAfter(LocalDateTime.now());
-    }
-
-    public void deleteToken(Long userId) {
-        refreshTokenRepository.deleteByUserId(userId);
-    }
+    }*/
 }
